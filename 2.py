6fa6210e98724a14de395e53cc803f99fe0f441d
Daniel Garcia <zlapper@gmail.com>
2011-10-02 14:50:05 -0500

    Problem #2

---
+++ 2.py
@@ -0,0 +1,12 @@
+##Problem 2
+##By considering the terms in the Fibonacci sequence whose values do not
+##exceed four million, find the sum of the even-valued terms.
+
+def fibo(maxnumber, number_list=[1,2]):
+    assert(number_list and len(number_list)>=2)
+    while number_list[-2]+number_list[-1] <= maxnumber:
+        number_list.append(number_list[-2]+number_list[-1])
+    return number_list
+def problem2(limit=4000000):
+    fibonacci_numbers = [1,2]
+    return sum(x for x in fibo(limit, fibonacci_numbers) if x%2 == 0)
